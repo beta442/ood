a083a978d03cbd54207e0b502d21a7a98e496231
beta442 <grandoblivioniv@gmail.com>
2022-09-11 20:41:42 +0300

    observer: observer_duo init

---
+++ lab2/observer_duo/src/WeatherData.cpp
@@ -0,0 +1,46 @@
+#include "../include/WeatherData.h"
+
+WeatherData::WeatherData()
+	: m_temperature(0.0)
+	, m_humidity(0.0)
+	, m_pressure(760.0)
+{
+}
+
+double WeatherData::GetTemperature() const
+{
+	return m_temperature;
+}
+
+double WeatherData::GetHumidity() const
+{
+	return m_humidity;
+}
+
+double WeatherData::GetPressure() const
+{
+	return m_pressure;
+}
+
+void WeatherData::MeasurementsChanged()
+{
+	NotifyObservers();
+}
+
+void WeatherData::SetMeasurements(double temp, double humidity, double pressure)
+{
+	m_humidity = humidity;
+	m_temperature = temp;
+	m_pressure = pressure;
+
+	MeasurementsChanged();
+}
+
+WeatherInfo WeatherData::GetChangedData() const
+{
+	WeatherInfo info;
+	info.temperature = GetTemperature();
+	info.humidity = GetHumidity();
+	info.pressure = GetPressure();
+	return info;
+}
