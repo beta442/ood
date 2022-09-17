98a0610e77092b9340a371b4c9c881a3755f7c70
beta442 <grandoblivioniv@gmail.com>
2022-09-17 17:37:49 +0300

    observer_safe_update: renamed `boost_test_module`

--- lab2/test/observer_safe_update_test/include/pch.h
+++ lab2/test/observer_safe_update_test/include/pch.h
@@ -1,7 +1,7 @@
 #ifndef PCH_H
 #define PCH_H
 
-#define BOOST_TEST_MODULE ObserverTests
+#define BOOST_TEST_MODULE ObserverSafeUpdateTests
 #include <boost/test/included/unit_test.hpp>
 
 #endif // !PCH_H
