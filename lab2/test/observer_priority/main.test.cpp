8620f8a44584846618851bad7728761029ed5749
beta442 <grandoblivioniv@gmail.com>
2022-09-19 17:34:45 +0300

    observer_priority: refactor after lab-lesson

---
+++ lab2/test/observer_priority/main.test.cpp
@@ -0,0 +1,22 @@
+#include "include/pch.h"
+
+#include "include/ObserverPriority.h"
+
+BOOST_AUTO_TEST_SUITE(ObserverPriority);
+
+BOOST_AUTO_TEST_CASE(UpdateObserversInNormalOrder)
+{
+	ValidatePriorityOrder<std::greater<size_t>>(CompareResult::GREATER);
+}
+
+BOOST_AUTO_TEST_CASE(UpdateObserversInReversedOrder)
+{
+	ValidatePriorityOrder<std::less<size_t>>(CompareResult::LESS);
+}
+
+BOOST_AUTO_TEST_CASE(UpdateObserversWithEqualPriority)
+{
+	ValidatePriorityOrder<std::greater<size_t>>(CompareResult::EQUAL, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
