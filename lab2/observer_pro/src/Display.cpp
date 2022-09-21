d72d57021e0098e270525b0e91507922467b307d
beta442 <grandoblivioniv@gmail.com>
2022-09-21 20:10:21 +0300

    observer_pro: refactor

--- lab2/observer_pro/src/Display.cpp
+++ lab2/observer_pro/src/Display.cpp
@@ -7,6 +7,8 @@ void Display::Update(const WeatherInfo& data, const IObservable<WeatherInfo>& up
 	std::cout << "Current Temp " << data.temperature << std::endl;
 	std::cout << "Current Hum " << data.humidity << std::endl;
 	std::cout << "Current Pressure " << data.pressure << std::endl;
+	std::cout << "Current Wind angle" << data.windInfo.windAngle << std::endl;
+	std::cout << "Current Wind speed" << data.windInfo.windSpeed << std::endl;
 	std::cout << "----------------" << std::endl;
 }
 
