15d8ad7c32109a02497461f0b917dbeb8cfda849
beta442 <grandoblivioniv@gmail.com>
2022-09-25 21:36:00 +0300

    observer_pro_duo_signals: removed commented code

--- lab2/observer_pro_duo_signals/main.cpp
+++ lab2/observer_pro_duo_signals/main.cpp
@@ -25,24 +25,5 @@ int main()
 	wdOut.SetMeasurements({ 40, 355, 3, 0.7, 760 });
 	wdOut.SetMeasurements({ 900, 359, 3, 0.7, 760 });
 
-	/*WeatherData<false> wdIn;
-	WeatherData wdOut;
-
-	wdOut.RegisterObserver(windSpeedDisplay);
-	wdOut.RegisterObserver(windAngleDisplay);
-
-	DisplayDuo display;
-	wdIn.RegisterObserver(display);
-	wdOut.RegisterObserver(display);
-
-	StatsDisplay statsDisplay;
-	wdIn.RegisterObserver(statsDisplay);
-	wdOut.RegisterObserver(statsDisplay);
-
-	wdIn.SetMeasurements({ 3, 0.7, 760 });
-	wdIn.SetMeasurements({ 3, 0.7, 760 });
-	wdOut.SetMeasurements({ 40, 355, 3, 0.7, 760 });
-	wdOut.SetMeasurements({ 900, 359, 3, 0.7, 760 });*/
-
 	return 0;
 }
