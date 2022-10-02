c06da359a303be71f96dbb6242f669682eee152f
beta442 <grandoblivioniv@gmail.com>
2022-10-02 21:29:22 +0300

    streams: refactor main

--- lab3/streams/main.cpp
+++ lab3/streams/main.cpp
@@ -7,27 +7,23 @@ int main(int argc, char* argv[])
 {
 	auto program = ParseArgs(argc, argv);
 
+	auto& inputFileName = program.get(INPUT_FILE_PAR);
+	auto& outputFileName = program.get(OUTPUT_FILE_PAR);
+
+	auto decryptKeys = program.get<std::vector<unsigned char>>(DECRYPT_FLAG);
+	auto encryptKeys = program.get<std::vector<unsigned char>>(ENCRYPT_FLAG);
+
+	bool shouldCompress = program.get<bool>(ÑOMPRESS_FLAG);
+	bool shouldDeCompress = program.get<bool>(DECOMPRESS_FLAG);
+
 	try
 	{
-		auto& inputFileName = program.get(INPUT_FILE_PAR);
-		auto& outputFileName = program.get(OUTPUT_FILE_PAR);
-
-		auto decryptKeys = program.get<std::vector<unsigned char>>(DECRYPT_FLAG);
-		auto encryptKeys = program.get<std::vector<unsigned char>>(ENCRYPT_FLAG);
-
-		bool shouldCompress = program.get<bool>(ÑOMPRESS_FLAG);
-		bool shouldDeCompress = program.get<bool>(DECOMPRESS_FLAG);
-
 		auto inputFileHandler = CreateInputFileHandler(inputFileName, decryptKeys, shouldDeCompress);
 		auto outputFileHandler = CreateOutputFileHandler(outputFileName, encryptKeys, shouldCompress);
 
-		while (true)
+		while (!inputFileHandler->IsEOF())
 		{
 			auto byte = inputFileHandler->ReadByte();
-			if (inputFileHandler->IsEOF())
-			{
-				break;
-			}
 			outputFileHandler->WriteByte(byte);
 		}
 	}
