6537bec50f90eccaa7c9c3aebc4379edbacef435
beta442 <grandoblivioniv@gmail.com>
2022-10-22 22:56:45 +0300

    command: `Menu` impl

--- lab5/include/Menu/Menu.h
+++ lab5/include/Menu/Menu.h
@@ -1,13 +1,46 @@
 #ifndef COMMAND_MENU_MENU_H_
 #define COMMAND_MENU_MENU_H_
 
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "../Commands/IUndoableEdit.h"
 
 class Menu
 {
 public:
+	using Command = std::function<void(std::istream&)>;
+
+	Menu(std::istream& inputS, std::ostream& outputS);
+
+	void AddItem(const std::string& shortcut, const std::string& dscrpt, const Command& command);
+	void Run();
+	void ShowInstructions();
+	void Exit();
 
 private:
+	bool ExecuteCommand(const std::string& command);
+
+	struct Item
+	{
+		Item(const std::string& shortcut, const std::string& description, const Command& command)
+			: shortcut(shortcut)
+			, description(description)
+			, command(command)
+		{
+		}
+
+		std::string shortcut{};
+		std::string description{};
+		Command command{};
+	};
+
+	std::istream& m_input;
+	std::ostream& m_output;
+
+	std::vector<Item> m_items{};
 	bool m_exit = false;
 };
 
---
+++ lab5/src/Menu/Menu.cpp
@@ -0,0 +1,62 @@
+#include "../../include/Menu/Menu.h"
+
+#include <sstream>
+
+Menu::Menu(std::istream& inputS, std::ostream& outputS)
+	: m_input(inputS)
+	, m_output(outputS)
+{
+}
+
+void Menu::AddItem(const std::string& shortcut, const std::string& dscrpt, const Command& command)
+{
+	m_items.emplace_back(shortcut, dscrpt, command);
+}
+
+void Menu::Run()
+{
+	ShowInstructions();
+
+	std::string command;
+	while ((m_output << ">")
+		&& std::getline(m_input, command)
+		&& ExecuteCommand(command))
+	{
+	}
+}
+
+void Menu::ShowInstructions()
+{
+	m_output << "Commands list:\n";
+	for (auto& item : m_items)
+	{
+		m_output << "--" << item.shortcut << ": " << item.description << "\n";
+	}
+}
+
+void Menu::Exit()
+{
+	m_exit = true;
+}
+
+bool Menu::ExecuteCommand(const std::string& command)
+{
+	std::istringstream iss(command);
+	std::string name;
+	iss >> name;
+
+	m_exit = false;
+	auto it = std::find_if(m_items.begin(), m_items.end(), [&name](const Item& item) {
+		return item.shortcut == name;
+	});
+
+	if (it != m_items.end())
+	{
+		it->command(iss);
+	}
+	else
+	{
+		m_output << "Unknown command\n";
+	}
+	return !m_exit;
+}
