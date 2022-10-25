3ae392b85077dd72d0dac57d9ca84c72b985b5a0
beta442 <grandoblivioniv@gmail.com>
2022-10-26 01:34:44 +0300

    command: iterator success attempt (not stl iterator)

---
+++ lab5/include/Iterator/Iterator.hpp
@@ -0,0 +1,55 @@
+#ifndef COMMAND_ITERATOR_ITERATOR_HPP_
+#define COMMAND_ITERATOR_ITERATOR_HPP_
+
+#include <memory>
+#include <iterator>
+#include <stdexcept>
+
+template <typename T>
+class IIterator
+{
+public:
+	using reference_type = T&;
+
+	virtual bool HasNext() const = 0;
+	virtual reference_type Get() const = 0;
+	virtual void Next() = 0;
+};
+
+template <typename T>
+using IIteratorPtr = std::unique_ptr<IIterator<T>>;
+
+template <typename T, typename IteratorT>
+class IteratorWrapper : public IIterator<T>
+{
+public:
+	using MyBase = IIterator<T>;
+
+	using reference_type = typename MyBase::reference_type;
+
+	IteratorWrapper(IteratorT begin, IteratorT end)
+		: m_it(begin)
+		, m_end(end)
+	{
+	}
+
+	bool HasNext() const override
+	{
+		return m_it != m_end;
+	}
+
+	reference_type Get() const override
+	{
+		return *m_it;
+	}
+
+	void Next() override
+	{
+		++m_it;
+	}
+
+private:
+	IteratorT m_it, m_end;
+};
+
+#endif // !COMMAND_ITERATOR_ITERATOR_HPP_
