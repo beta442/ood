c04f276d33c58b623dcb08bef968f92e71a71b88
beta442 <grandoblivioniv@gmail.com>
2022-10-31 02:09:36 +0300

    command-test: add `Editor` tests. Linked `editor` with `editor_test`

---
+++ lab5/editor/test/unit/Editor.test.cpp
@@ -0,0 +1,11 @@
+#include "../include/pch.h"
+
+#include "../../../editor/include/Editor/Editor.h"
+
+BOOST_AUTO_TEST_SUITE(Editor_tests)
+
+	BOOST_AUTO_TEST_CASE(Create_editor_tests)
+	{
+	}
+
+BOOST_AUTO_TEST_SUITE_END();
